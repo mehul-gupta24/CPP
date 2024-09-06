bed272a1f81a8987177bacd930695162b41a6cac
Mehul Gupta <mehulgupta2409@Mehuls-MacBook-Air.local>
2024-09-07 00:09:00 +0530

    2

---
+++ Binar Search/MedianOfTwoSortedArrays.cpp
@@ -0,0 +1,184 @@
+// 4. Median of Two Sorted Arrays
+// Solved
+// Hard
+// Topics
+// Companies
+// Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+
+// The overall run time complexity should be O(log (m+n)).
+
+ 
+
+// Example 1:
+
+// Input: nums1 = [1,3], nums2 = [2]
+// Output: 2.00000
+// Explanation: merged array = [1,2,3] and median is 2.
+// Example 2:
+
+// Input: nums1 = [1,2], nums2 = [3,4]
+// Output: 2.50000
+// Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
+ 
+
+// Constraints:
+
+// nums1.length == m
+// nums2.length == n
+// 0 <= m <= 1000
+// 0 <= n <= 1000
+// 1 <= m + n <= 2000
+// -106 <= nums1[i], nums2[i] <= 106
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class mySolution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        vector<double>ans;
+        for(int i:nums1){
+            ans.push_back(i);
+        }
+        for(int i:nums2){
+            ans.push_back(i);
+        }
+        for(int i:ans){
+            cout<<i<<" ";
+        }
+        sort(ans.begin(),ans.end());
+        int n=ans.size();
+        if(ans.size()%2==0){
+            return (ans[n/2]+ans[(n-1)/2])/2;
+        }
+        return ans[ans.size()/2];
+    }
+};
+
+
+class editorialSolution_1 {
+public:
+    int p1 = 0, p2 = 0;
+
+    // Get the smaller value between nums1[p1] and nums2[p2] and move the
+    // pointer forward.
+
+    int getMin(vector<int>& nums1, vector<int>& nums2) {
+        if (p1 < nums1.size() && p2 < nums2.size()) {
+            return nums1[p1] < nums2[p2] ? nums1[p1++] : nums2[p2++];
+        } else if (p1 < nums1.size()) {
+            return nums1[p1++];
+        } else if (p2 < nums2.size()) {
+            return nums2[p2++];
+        }
+        return -1;
+    }
+
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int m = int(nums1.size()), n = int(nums2.size());
+
+        if ((m + n) % 2 == 0) {
+            for (int i = 0; i < (m + n) / 2 - 1; ++i) {
+                int _ = getMin(nums1, nums2);
+            }
+            return (double)(getMin(nums1, nums2) + getMin(nums1, nums2)) / 2;
+        } else {
+            for (int i = 0; i < (m + n) / 2; ++i) {
+                int _ = getMin(nums1, nums2);
+            }
+            return getMin(nums1, nums2);
+        }
+
+        return -1;
+    }
+};
+
+class editorialSolution_2_binarySearch {
+public:
+    double findMedianSortedArrays(vector<int>& A, vector<int>& B) {
+        int na = int(A.size()), nb = int(B.size());
+        int n = na + nb;
+        if (n % 2) {
+            return solve(A, B, n / 2, 0, na - 1, 0, nb - 1);
+        } else {
+            return 1.0 *
+                   (solve(A, B, n / 2 - 1, 0, na - 1, 0, nb - 1) +
+                    solve(A, B, n / 2, 0, na - 1, 0, nb - 1)) /
+                   2;
+        }
+    }
+    int solve(vector<int>& A, vector<int>& B, int k, int aStart, int aEnd,
+              int bStart, int bEnd) {
+        // If the segment of on array is empty, it means we have passed all
+        // its element, just return the corresponding element in the other
+        // array.
+        if (aEnd < aStart) {
+            return B[k - aStart];
+        }
+        if (bEnd < bStart) {
+            return A[k - bStart];
+        }
+
+        // Get the middle indexes and middle values of A and B.
+        int aIndex = (aStart + aEnd) / 2, bIndex = (bStart + bEnd) / 2;
+        int aValue = A[aIndex], bValue = B[bIndex];
+
+        // If k is in the right half of A + B, remove the smaller left half.
+        if (aIndex + bIndex < k) {
+            if (aValue > bValue) {
+                return solve(A, B, k, aStart, aEnd, bIndex + 1, bEnd);
+            } else {
+                return solve(A, B, k, aIndex + 1, aEnd, bStart, bEnd);
+            }
+        }
+        // Otherwise, remove the larger right half.
+        else {
+            if (aValue > bValue) {
+                return solve(A, B, k, aStart, aIndex - 1, bStart, bEnd);
+            } else {
+                return solve(A, B, k, aStart, aEnd, bStart, bIndex - 1);
+            }
+        }
+        return -1;
+    }
+};
+
+
+class editorialSolution_3_binarySearch {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        if (nums1.size() > nums2.size()) {
+            return findMedianSortedArrays(nums2, nums1);
+        }
+
+        int m = nums1.size(), n = nums2.size();
+        int left = 0, right = m;
+
+        while (left <= right) {
+            int partitionA = (left + right) / 2;
+            int partitionB = (m + n + 1) / 2 - partitionA;
+
+            int maxLeftA = (partitionA == 0) ? INT_MIN : nums1[partitionA - 1];
+            int minRightA = (partitionA == m) ? INT_MAX : nums1[partitionA];
+            int maxLeftB = (partitionB == 0) ? INT_MIN : nums2[partitionB - 1];
+            int minRightB = (partitionB == n) ? INT_MAX : nums2[partitionB];
+
+            if (maxLeftA <= minRightB && maxLeftB <= minRightA) {
+                if ((m + n) % 2 == 0) {
+                    return (max(maxLeftA, maxLeftB) +
+                            min(minRightA, minRightB)) /
+                           2.0;
+                } else {
+                    return max(maxLeftA, maxLeftB);
+                }
+            } else if (maxLeftA > minRightB) {
+                right = partitionA - 1;
+            } else {
+                left = partitionA + 1;
+            }
+        }
+
+        return 0.0;
+    }
+};
