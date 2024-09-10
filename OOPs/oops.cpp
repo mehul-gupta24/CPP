0ba61c5151b308da48951391672b6208addbf6a5
Mehul Gupta <mehulgupta2409@Mehuls-MacBook-Air.local>
2024-09-10 19:16:05 +0530

    s

--- OOPs/oops.cpp
+++ OOPs/oops.cpp
@@ -22,12 +22,21 @@ class Person{
     public:
     string name;
     int age;
+    Person(){
+        cout<<"I am constructor - Person"<<endl;
+    }
+    Person(string name,int age){
+        cout<<"I am constructor - Person"<<endl;
+    }
+    ~Person(){
+        cout<<"Hi ,i am destructor of person"<<endl;
+    }
     // Person(string name,int age){
     //     this->age=age;
     //     this->name=name;
     // }
-
 };
+
 //child/derived class ,Inherit
 class Student : public Person{
     public:
@@ -37,11 +46,18 @@ class Student : public Person{
     // Student(int val){
     //     this->roll_no=val;
     // }
+    Student(string name,int age,string roll_no) : Person(name,age) {
+        this->roll_no=roll_no;
+        cout<<"i am child constructor - student"<<endl;
+    }
     void get_info(){
         cout<<"Name : "<< this->name<<endl;
         cout<<"Age : "<< this->age<<endl;
         cout<<"Roll-no : "<< this->roll_no<<endl;
         return;
+    }
+    ~Student(){
+        cout<<"Hi ,i am destructor of student"<<endl;
     }
 };
 
@@ -49,10 +65,12 @@ int main() {
     // Student s_1(221,'o');
     // cout<<*(s_1.data)<<endl;
     // cout<<*(s_1.ch);
-    Student s;
-    s.name="Mehul";
-    s.age=21;
-    s.roll_no="ME22BT024";
-    s.get_info();
+    // Student s;
+    // s.name="Mehul";
+    // s.age=21;
+    // s.roll_no="ME22BT024";
+    // s.get_info();
+    Student s("m",21,"Me22bt024");
+    
     return 0;
 }
