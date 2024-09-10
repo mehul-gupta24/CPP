e8aaff182fa25d1a57d3f84735e9a433adb557dc
Mehul Gupta <mehulgupta2409@Mehuls-MacBook-Air.local>
2024-09-10 22:30:34 +0530

    w

--- Graph/Learning/bfs.cpp
+++ Graph/Learning/bfs.cpp
@@ -1,5 +1,4 @@
 // #include<bits/stdc++.h>
-// #include <bits/stdc++.h>
 #include<iostream>
 #include<vector>
 #include<map>
---
+++ Linked List/LL.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+class Node{
+    public:
+    int val;
+    Node*next;
+    Node(int val){
+        this->val=val;
+        this->next=NULL;
+    }
+    Node* createLL(int val){
+        Node*head=new Node(val);
+    }
+    void add(int val){
+        if(val==-1){
+            return;
+        }
+        Node*head=new Node(val);
+        head->next=createLL();
+    }
+};
+int main(){
+    Node* head(5);
+    Node* head(5);
+}
