7bf0713ab2fd81198522dc4c43f4f01b46cc791a
Mehul Gupta <mehulgupta2409@Mehuls-MacBook-Air.local>
2024-09-13 19:36:37 +0530

    de

--- Graph/Learning/bfs.cpp
+++ Graph/Learning/bfs.cpp
@@ -3,6 +3,7 @@
 #include<vector>
 #include<map>
 #include<queue>
+// #include <utility>
 #include<list>
 using namespace std;
 class Graph{
@@ -72,6 +73,7 @@ class Graph{
         return ans;
     }
     bool cycle_bfs(unordered_map<int,list<int>>&adj,unordered_map<int,bool>&visited,unordered_map<int,int>&parent,int node){
+        //undirected
         queue<int>q;
         visited[node]=true;
         parent[node]=-1;
@@ -80,7 +82,7 @@ class Graph{
             int front=q.front();
             q.pop();
             for(auto it:adj[front]){
-                if(parent[it]!=front && visited[it]){
+                if(parent[front]!=it && visited[it]){
                     return true;
                 }
                 else if(!visited[it]){
@@ -90,6 +92,45 @@ class Graph{
                 }
             }
         }
+        return false;
+    }
+    bool cycle_dfs(unordered_map<int,list<int>>&adj,unordered_map<int,bool>&visited,int node,int parent){
+        //undirected
+        visited[node]=true;
+        for(auto neighbour:adj[node]){
+            if(!visited[neighbour]){
+                bool a=cycle_dfs(adj,visited,neighbour,node);
+                if(a){
+                    return true;
+                }
+            }
+            else if(neighbour!=parent){
+                return true;
+            }
+        }
+        return false;
+
+    }
+    bool cycleDetectionByDFS(vector<vector<int>>edges,int vertex,int edge,int source_node){
+        //undirected
+        unordered_map<int,list<int>>adj;
+        for(int i=0;i<edges.size();i++){
+            int u=edges[i][0];
+            int v=edges[i][1];
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        unordered_map<int,bool>visited;
+        for(int i=0;i<vertex;i++){
+            if(!visited[i]){
+                bool ans=cycle_dfs(adj,visited,i,-1);
+                if(ans) {
+                    cout<<"cycle is present";
+                    return true;
+                }
+            }
+        }
+        cout<<"cycle is not present";
         return false;
     }
     bool cycleDetectionByBFS(vector<vector<int>>edges,int vertex,int edge,int source_node){
@@ -115,6 +156,38 @@ class Graph{
         cout<<"cycle is not present";
         return false;
     }
+    bool check_cycle_directed(unordered_map<int,list<int>>&adj,unordered_map<int,bool>&visited,unordered_map<int,bool>&dfs_visited,int node){
+        visited[node]=true;
+        dfs_visited[node]=true;
+        for(auto neighbour:adj[node]){
+            if(!visited[neighbour]){
+                bool ans=check_cycle_directed(adj,visited,dfs_visited,neighbour);
+                if(ans)
+                    return true;
+            }
+            else if(dfs_visited[neighbour]){
+                return true;
+            }
+        }
+        dfs_visited[node]=false;
+        return false;
+    }
+    bool cycleDetectionByDFS_directed(vector<pair<int,int>>edges,int source,int edge,int vertices){
+        unordered_map<int,list<int>>adj;
+        for(int i=0;i<edges.size();i++){
+            int u=edges[i].first;
+            int v=edges[i].second;
+            adj[u].push_back(v);
+        }
+        unordered_map<int,bool>visited;
+        unordered_map<int,bool>dfs_visited;
+        for(int i=0;i<vertices;i++){
+            if(!visited[i]){
+                return check_cycle_directed(adj,visited,dfs_visited,i);
+            }
+        }
+        return false;
+    }
 
 };
 int main(){
@@ -123,8 +196,18 @@ int main(){
     // vector<int>ans=g.solve_for_bfs({{1,2},{2,3},{3,4},{4,1}},4,4,1);
     // vector<int>ans2=g.solve_for_bfs({{1,2},{2,3},{3,4},{4,1},{2,4}},4,5,1);
     // vector<int>ans=g.solve_for_dfs({{0, 2}, {0, 3}, {0, 1}, {2, 4}},5,4,0);
-    g.cycleDetectionByBFS({{0, 1}, {1, 2}, {2, 4}, {0, 3}},5,4,0);
+    // g.cycleDetectionByDFS({{0, 1}, {1, 2}, {2, 4}, {0, 2}},4,4,0);
+    // g.cycleDetectionByDFS({{0, 1}, {1, 2}, {2, 4}, {0, 3}},5,4,0);
     
+    // bool ans=g.cycleDetectionByDFS_directed({{0,1},{1,2},{2,3}},0,3,4);
+    // if(ans){
+    //     cout<<"cycle present";
+    //     return 0;
+    // }
+    // cout<<"cycle is not present";
+
+    
+
     // for(int i=0;i<ans.size();i++){
     //     cout<<ans[i]<<" ";
     // }
