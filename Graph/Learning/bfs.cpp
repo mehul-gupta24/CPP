8479286451427bc9767674bfca0f98e5f6d23201
Mehul Gupta <mehulgupta2409@Mehuls-MacBook-Air.local>
2024-10-03 17:41:31 +0530

    s

--- Graph/Learning/bfs.cpp
+++ Graph/Learning/bfs.cpp
@@ -459,8 +459,53 @@ class Graph{
         }
     }
     // prims algorithm is used to find the minimum spanning tree 
-    // now what is MST(minimum spanning tree)
+    // spanning tree - when u convert graph into tree(means no cycle - means graph with no cycle) - n nodes , n-1 edges , every node is reachable to any other node
+
+    // now what is MST(minimum spanning tree) - spanning tree in minimum cost of weights
+    vector<pair<pair<int,int>,int>>calculatePrimsMST(int n,int m,vector<pair<pair<int,int>,int>>g){
+        unordered_map<int,list<pair<int,int>>>adj;
+        for(int i=0;i<g.size();i++){
+            int u=g[i].first.first;
+            int v=g[i].first.second;
+            int w=g[i].second;
+            adj[u].push_back({v,w});
+            adj[v].push_back({u,w});
+        }
+        vector<int>key(n+1,INT_MAX);
+        vector<bool>mst(n+1,false);
+        vector<int>parent(n+1,-1);
+        key[1]=0;
+        parent[1]=-1;
+        mst[-1]=false;
+        for(int i=1;i<=n;i++){
+            int u;
+            int mini=INT_MAX;
+            //find minimum wali node
+            for(int v=1;v<=n;v++){
+                if(mst[v]==false && key[v]<mini){
+                    u=v;
+                    mini=key[v];
+                }
+            }
+            //mark min node as true;
+            mst[u]=true;
+            for(auto it:adj[u]){
+                int v=it.first;
+                int w=it.second;
+                if(mst[v]==false && w<key[v]){
+                    parent[v]=u;
+                    key[v]=w;
+                }
+            }
+        }
+        vector<pair<pair<int,int>,int>>ans;
+        for(int i=2;i<=n;i++){
+            ans.push_back({{parent[i],i},key[i]});
+        }
+        return ans;
+    }
     
+
 };
 int main(){
     // int vertex=3;
